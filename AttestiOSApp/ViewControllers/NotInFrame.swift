ee5789f75fa060a35a7b90623424476778d76eb4
Chris McMeeking <chris.mcmeeking@deque.com>
2017-11-06 13:32:21 -0500

    Removed framework. Added script to fetch framework from Agora. Added circle.yml configuration.

--- AttestiOSApp/ViewControllers/NotInFrame.swift
+++ AttestiOSApp/ViewControllers/NotInFrame.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Attest
 
 class NotInFrame: UIViewController, UIScrollViewDelegate {
 
---
+++ Scripts/CurlFramework.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Change this to the version of the framework you want.
+FRAMEWORK_VERSION=1.1.0_rc1
+
+#This has to match up with Fraqmework Search paths in XCode Build Configuration.
+FRAMEWORK_DIR=./Framework/Release
+
+#Ensure it exists and change to it.
+mkdir -p $FRAMEWORK_DIR
+pushd $FRAMEWORK_DIR
+
+#Remove any artifacts leftover from last time.
+rm -f Attest.framework*.zip
+rm -rf Attest.framework
+
+#Fetch the library zip with our anonymous api key.
+curl -H "X-JFrog-Art-Api: AKCp5Z3qFNjBcw3yvfWA7rcmT7Yf4wcR1FTE5P5VBubBfd7TrURFKJvBQiPcUFAAUvEgavrLg" \
+	-O "https://agora.dequecloud.com/artifactory/Attest-iOS/com/deque/woldspace/attest-ios-framework/$FRAMEWORK_VERSION/Attest.framework-$FRAMEWORK_VERSION.zip"
+
+#Unzip whatever version was fetched
+unzip Attest.framework*.zip
