5092e2a41e4a75eaf70dc1cefd02ff60e1d9f356
Chris McMeeking <chris.mcmeeking@deque.com>
2017-11-08 12:46:58 -0500

    Silly plugin version updates.

--- AttestiOSApp/AppDelegate.swift
+++ AttestiOSApp/AppDelegate.swift
@@ -14,21 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool { return true }
-
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
     func applicationDidBecomeActive(_ application: UIApplication) {
         //Start the attest server in debug mode to use the desktop client.
-        #if DEBUG
-            Attest.startServer(48484)
-        #endif
+        Attest.startServer(48484)
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {}
 }
 
