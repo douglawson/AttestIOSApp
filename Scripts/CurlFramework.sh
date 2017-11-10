f618b627ad78b3cc2462ebbcd5b490be6780779f
Chris McMeeking <chris.mcmeeking@deque.com>
2017-11-10 16:08:48 -0500

    Update to use the 1.1.0 version of the framework.

--- Scripts/CurlFramework.sh
+++ Scripts/CurlFramework.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Change this to the version of the framework you want.
-FRAMEWORK_VERSION=1.1.0_rc1
+FRAMEWORK_VERSION=1.1.0
 
 
 #The version of Xcode you want to fetch the framework for (Xcode9.0 or Xcode9.1)
 #The Xcode9.0 framework build should work for 8.0 - 9.0.1. If you need another build post an issue.
 XCODE_VERSION=Xcode9.1
 
-#This has to match up with Fraqmework Search paths in XCode Build Configuration.
+#This directory has to match up with your Fraqmework Search paths in XCode Build Configuration.
 FRAMEWORK_DIR=./Framework/Release
 
 #Ensure it exists and change to it.
@@ -18,7 +18,7 @@ pushd $FRAMEWORK_DIR
 
 #Fetch the library zip with our anonymous api key.
 curl -H "X-JFrog-Art-Api: AKCp5Z3qFNjBcw3yvfWA7rcmT7Yf4wcR1FTE5P5VBubBfd7TrURFKJvBQiPcUFAAUvEgavrLg" \
-	-O "https://agora.dequecloud.com/artifactory/AttestIOSFree/framework/$XCODE_VERSION/$FRAMEWORK_VERSION/Attest.framework-$FRAMEWORK_VERSION.zip"
+	-O "https://agora.dequecloud.com/artifactory/AttestIOSFree/framework/$XCODE_VERSION/Attest.framework-$FRAMEWORK_VERSION.zip"
 
 #Unzip whatever version was fetched
 unzip Attest.*zip
